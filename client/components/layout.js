c0af14eb52b33f16026144c3e6c8d74c6e6f34bd
kevinrodrigues <kevrodrigues116@gmail.com>
2017-03-19 11:14:13 +0000

    Updated bind function.

--- client/components/layout.js
+++ client/components/layout.js
@@ -3,15 +3,9 @@ import $ from "jquery";
 
 export default class Layout extends React.Component {
 
-    getDefaults() {
-        return {
-
-        }
-    }
-
     getLiveTimes(url) {
         return $.ajax({
-            url: "https://transportapi.com/v3/uk/cycle/journey/from/postcode:IG62HJ/to/postcode:IG61EG.json?app_id=03bf8009&app_key=d9307fd91b0247c607e098d5effedc97"
+            url: url
         }).then(function(data) {
             console.log(data);
         }).fail(function() {
@@ -22,12 +16,10 @@ export default class Layout extends React.Component {
     render() {
         return (
             <div className="liveTimes">
-                <div className="times"></div>
-                <button onClick={this.getLiveTimes.bind(this)}>Get Times</button>
+                <input type="text" placeholder="From"/>
+                <input type="text" placeholder="To"/>
+                <button onClick={() => this.getLiveTimes('https://transportapi.com/v3/uk/cycle/journey/from/postcode:IG62HJ/to/postcode:IG61EG.json?app_id=03bf8009&app_key=d9307fd91b0247c607e098d5effedc97')}>Get Cycle Route</button>
             </div>
         );
     }
 }
-
-
-
