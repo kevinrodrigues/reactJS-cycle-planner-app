06c4fe0620e13c70ba64904e35a343ab1ce953d0
kevinrodrigues <kevrodrigues116@gmail.com>
2017-03-19 10:28:37 +0000

    Updated layout.js

--- client/components/layout.js
+++ client/components/layout.js
@@ -3,12 +3,19 @@ import $ from "jquery";
 
 export default class Layout extends React.Component {
 
+    getDefaults() {
+        return {
+
+        }
+    }
+
     getLiveTimes(url) {
         return $.ajax({
             url: "https://transportapi.com/v3/uk/train/station/WAT/live.json?app_id=03bf8009&app_key=d9307fd91b0247c607e098d5effedc97&darwin=false&train_status=passenger"
         }).then(function(data) {
-            // $('.times').text(JSON.stringify(data));
             console.log(data);
+        }).fail(function() {
+            console.log('Failed to fetch resource');
         });
     }
 
