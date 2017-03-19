a3d59117d6bfc882328ebe9b30c7e955ef8fb82d
kevinrodrigues <kevrodrigues116@gmail.com>
2017-03-19 10:43:09 +0000

    Updated cycle planner.

--- client/components/layout.js
+++ client/components/layout.js
@@ -11,7 +11,7 @@ export default class Layout extends React.Component {
 
     getLiveTimes(url) {
         return $.ajax({
-            url: "https://transportapi.com/v3/uk/train/station/WAT/live.json?app_id=03bf8009&app_key=d9307fd91b0247c607e098d5effedc97&darwin=false&train_status=passenger"
+            url: "https://transportapi.com/v3/uk/cycle/journey/from/postcode:IG62HJ/to/postcode:IG61EG.json?app_id=03bf8009&app_key=d9307fd91b0247c607e098d5effedc97"
         }).then(function(data) {
             console.log(data);
         }).fail(function() {
